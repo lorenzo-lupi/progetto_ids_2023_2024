6c823812d9c16654957f3f0e40e14e4501edea53
lorenzo <lupi.lorenzo001@gmail.com>
2024-01-27 23:16:39 +0100

    fix

--- java/app_valorizzazione_territorio_java/src/main/java/it/cs/unicam/app_valorizzazione_territorio/abstractions/ContentHost.java
+++ java/app_valorizzazione_territorio_java/src/main/java/it/cs/unicam/app_valorizzazione_territorio/abstractions/ContentHost.java
@@ -5,7 +5,7 @@ import it.cs.unicam.app_valorizzazione_territorio.model.Content;
 import java.util.Collection;
 
 public interface ContentHost<V extends ContentHost<V> & Visualizable> extends Identifiable {
-    Collection<Content<V>> getContents();
+    Collection<? extends Content<V>> getContents();
      default Collection<? extends Content<V>> getApprovedContents(){
         return getContents().stream().filter(Content::isApproved).toList();
      }
