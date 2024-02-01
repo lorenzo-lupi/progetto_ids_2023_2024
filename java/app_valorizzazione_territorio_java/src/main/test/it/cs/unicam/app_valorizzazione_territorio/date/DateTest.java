6badf17fbe66a16b0b3a5f61585cbd05196ffba2
lorenzo <lupi.lorenzo001@gmail.com>
2024-02-01 13:14:20 +0100

    new Date class

---
+++ java/app_valorizzazione_territorio_java/src/main/test/it/cs/unicam/app_valorizzazione_territorio/date/DateTest.java
@@ -0,0 +1,35 @@
+package it.cs.unicam.app_valorizzazione_territorio.date;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DateTest {
+    @Test
+    void shouldConstructDate() {
+        assertDoesNotThrow(() -> new Date("01-01-2021"));
+        assertDoesNotThrow(() -> new Date("31-12-2021"));
+        assertDoesNotThrow(() -> new Date("29-02-2020"));
+        assertDoesNotThrow(() -> new Date("28-02-2021"));
+        assertDoesNotThrow(() -> new Date("30-04-2011"));
+        assertDoesNotThrow(() -> new Date("31-05-2011"));
+        assertDoesNotThrow(() -> new Date("30-06-9999"));
+    }
+
+    @Test
+    void shouldNotConstructDate(){
+        assertThrows(IllegalArgumentException.class, () -> new Date("29-02-2021"));
+        assertThrows(IllegalArgumentException.class, () -> new Date("33-12-2021"));
+        assertThrows(IllegalArgumentException.class, () -> new Date("32-01-1999"));
+    }
+
+    @Test
+    void hashCodesShouldBeEqual(){
+        Date date1 = new Date("31-12-9999");
+        Date date2 = new Date("31-12-9999");
+        int hash1 = date1.hashCode();
+        int hash2 = date2.hashCode();
+        assertEquals(hash1, hash2);
+    }
+
+}
